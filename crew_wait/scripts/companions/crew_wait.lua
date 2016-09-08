8e3fe6ad4e4ca92601c63144f541bcec8048fa51
Tim Reynolds <Reynolds.TimJ@gmail.com>
2016-09-07 20:26:12 -0400

    Initial commit of module. Deploy to the mods directory of your Starbound installation.

---
+++ crew_wait/scripts/companions/crew_wait.lua
@@ -0,0 +1,96 @@
+function recruitable.confirmFollow(skipNotification)
+  sb.logInfo("Confirm Follow From Patch")
+  if not skipNotification then
+    local t = os.clock()
+    local first_run = false
+    if not self.last_follow then
+      sb.logInfo("First Run Confirm Follow From Patch")
+      first_run = true
+      self.last_follow = t
+    end
+    local d = t - self.last_follow
+    self.last_follow = t
+
+    if first_run then return end
+
+    self.last_follow = false
+
+    if d > 1 then return end
+  end
+
+  if not skipNotification then
+    local playerEntityId = world.loadUniqueEntity(recruitable.ownerUuid())
+    notify({ type = "follow", sourceId = playerEntityId})
+  end
+
+  npc.setPersistent(false)
+  npc.setKeepAlive(true)
+  storage.followingOwner = true
+  storage.behaviorFollowing = true
+  if playerEntityId and world.entityExists(playerEntityId) then
+    npc.setDamageTeam(recruitable.defaultDamageTeam)
+  end
+end
+
+
+function recruitable.confirmUnfollow(skipNotification)
+  sb.logInfo("Confirm Unfollow In My Patch")
+  if not skipNotification then
+    local t = os.clock()
+    local first_run = false
+    if not self.last_follow then
+      first_run = true
+      self.last_follow = t
+    end
+    local d = t - self.last_follow
+    self.last_follow = t
+
+    if first_run then return end
+
+    self.last_follow = false
+
+    if d > 1 then return end
+  end
+  if not skipNotification then
+    local playerEntityId = world.loadUniqueEntity(recruitable.ownerUuid())
+    notify({ type = "unfollow", sourceId = playerEntityId})
+  end
+
+  npc.setPersistent(true)
+  npc.setKeepAlive(false)
+  storage.followingOwner = false
+  storage.behaviorFollowing = false
+  npc.setDamageTeam(recruitable.defaultDamageTeam)
+end
+
+function recruitable.confirmUnfollowBehavior(skipNotification)
+
+  if not skipNotification then
+    local t = os.clock()
+    local first_run = false
+    if not self.last_follow then
+      first_run = true
+      self.last_follow = t
+    end
+    local d = t - self.last_follow
+    self.last_follow = t
+
+    if first_run then return end
+
+    self.last_follow = false
+
+    if d > 1 then return end
+  end
+  if not skipNotification then
+    local playerEntityId = world.loadUniqueEntity(recruitable.ownerUuid())
+    notify({ type = "unfollow", sourceId = playerEntityId})
+  end
+
+  npc.setPersistent(false)
+  npc.setKeepAlive(true)
+  storage.followingOwner = true
+  storage.behaviorFollowing = false
+  if playerEntityId and world.entityExists(playerEntityId) then
+    npc.setDamageTeam(recruitable.defaultDamageTeam)
+  end
+end
